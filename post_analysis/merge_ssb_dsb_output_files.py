3b61a0aac92bd0282217c96a0f1c917a6ba97068
VictorLevrague <victor.levrague@lpsc.in2p3.fr>
2025-01-16 15:12:58 +0100

    Updated file to cluster

--- post_analysis/merge_ssb_dsb_output_files.py
+++ post_analysis/merge_ssb_dsb_output_files.py
@@ -3,9 +3,11 @@ import os
 
 ION_ENERGY = "O_350_MeV"
 NB_SIMULATIONS = 1000
+PATH = "/sps/gdrmi2b/levrague/dsb_and_repair"
+IRRADIATION_ID = 1 #1 (HC nucleus) or 2 (decompacted nucleus)
 
 def merge_ssb_dsb_files():
-    analysis_output_folder = f"analysis_output/{ION_ENERGY}"
+    analysis_output_folder = f"{PATH}/post_analysis/analysis_output/{ION_ENERGY}/Irradiation{IRRADIATION_ID}"
     if not os.path.exists(analysis_output_folder):
         os.makedirs(analysis_output_folder)
     write_output_file("SSB", analysis_output_folder)
@@ -13,9 +15,10 @@ def merge_ssb_dsb_files():
 
 def write_output_file(damage_type, output_folder):
     with open(f"{output_folder}/List_{damage_type}.txt", "w") as output_file:
+        print(f"{output_folder}/List_{damage_type}.txt")
         output_file.write("0:run\t1:event\t2:chromosome\t3:domain\t4:genomic_position(bp)\t5:x(nm)\t6:y(nm)\t7:z(nm)\t8:complexity\t9:direct\t10:indirect\n")
         for copy_nb in range(0, NB_SIMULATIONS):
-            damage_file_name = f"output/output{ION_ENERGY}/Copy{copy_nb}/List_{damage_type}.txt"
+            damage_file_name = f"{PATH}/output/output{ION_ENERGY}/outputIrradiation{IRRADIATION_ID}/Copy{copy_nb}/List_{damage_type}.txt"
             with open(damage_file_name) as damage_file:
                 next(damage_file)
                 for line in damage_file:

41c7f95450b7e7e40d82ff75476642237da59352
VictorLevrague <victor.levrague@lpsc.in2p3.fr>
2025-01-16 10:00:55 +0100

    Updated clean_chem_input

--- submit_slurm/2_chem/clean_chem_input.sh
+++ submit_slurm/2_chem/clean_chem_input.sh
@@ -7,13 +7,14 @@
 export path=/sps/gdrmi2b/levrague/dsb_and_repair
 
 N=1000;
-export particle_energy="O_350"
+export particle_energy="H_150"
+export irradiation_id=1 #1 or 2
 
 N=`expr "$N" - 1`;
 
 for i in $(seq 0 $N)
 do
-  export output_folder_name=$path/output/output${particle_energy}_MeV/Copy$i/chem_input
+  export output_folder_name=$path/output/output${particle_energy}_MeV/outputIrradiation${irradiation_id}/Copy$i/chem_input
   echo $output_folder_name
   list_run=`ls $output_folder_name`
   rm -rf $output_folder_name

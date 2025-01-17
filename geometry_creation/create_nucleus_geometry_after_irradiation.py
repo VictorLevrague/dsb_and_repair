599494024ad60e5edcd53b55840f1d9a84e660c2
VictorLevrague <victor.levrague@lpsc.in2p3.fr>
2025-01-17 12:03:36 +0100

    Adapted geometry creation for Merged damage files usage

--- geometry_creation/create_nucleus_geometry_after_irradiation.py
+++ geometry_creation/create_nucleus_geometry_after_irradiation.py
@@ -3,8 +3,10 @@ import os
 import time
 from collections import defaultdict
 
-ion_energy = "O_350_MeV"
-nb_output_folder = 10
+PATH = "/sps/gdrmi2b/levrague/dsb_and_repair"
+ION_ENERGY = "O_350_MeV"
+DOSE = 1 #Gy
+NB_OUTPUT_FOLDER = 10
 
 def write_geometry_file(output_filename, tad_hits, header, hc_geom_dict, ec_geom_dict):
     hit_tads = read_hit_tads(tad_hits)
@@ -49,12 +51,12 @@ if __name__ == "__main__":
     voxels_tad_ec = read_geometry_files("geometrie_fibroblast_ec_floriane.fab2g4dna")[1]
     voxels_tad_hc_dict = group_by_tad_id(voxels_tad_hc)
     voxels_tad_ec_dict = group_by_tad_id(voxels_tad_ec)
-    ion_folder = f"output/output{ion_energy}"
+    ion_folder = f"output/output{ION_ENERGY}/{DOSE}Gy"
     if not os.path.exists(ion_folder):
         os.makedirs(ion_folder)
     start_time2 = time.time()
     print("time before loop: ", start_time2 - start_time1, " s")
-    for copy_nb in range(0, nb_output_folder):
-        write_geometry_file(ion_folder + f"/irradiated_geometry_file_{copy_nb}.fab2g4dna", f"/sps/gdrmi2b/levrague/dsb_and_repair/tad_hits/{ion_energy}/tad_hits_{copy_nb}.txt", header, voxels_tad_hc_dict, voxels_tad_ec_dict)
+    for copy_nb in range(0, NB_OUTPUT_FOLDER):
+        write_geometry_file(ion_folder + f"/irradiated_geometry_file_{copy_nb}.fab2g4dna", f"{PATH}/post_analysis/tad_hits/{ION_ENERGY}/{DOSE}Gy/tad_hits_{copy_nb}_ALL.txt", header, voxels_tad_hc_dict, voxels_tad_ec_dict)
         print(f"Geometry n°{copy_nb+1} created")
-    print("time per output: ", (time.time()-start_time2) / nb_output_folder, " s")
+    print("time per output: ", (time.time()-start_time2) / NB_OUTPUT_FOLDER, " s")
